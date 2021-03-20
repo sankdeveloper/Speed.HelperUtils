624ac6b7d3be6fef42acb3d8ed99ed47fe770998
sankdeveloper@gmail.com <sankdeveloper@gmail.com>
2021-03-20 23:08:15 +0530

    Add project files.

---
+++ Speed.HelperUtils/Extensions/QueryStringExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace Speed.HelperUtils.Extensions
+{
+    public static class QueryStringExtension
+    {
+        // get entire querystring name/value collection
+        public static NameValueCollection QueryString(this Uri url)
+        {
+            return HttpUtility.ParseQueryString(url.Query);
+        }
+
+        // get single querystring value with specified key
+        public static string QueryString(this Uri url, string key)
+        {
+            return url.QueryString()[key];
+        }
+
+        // get entire querystring name/value collection
+        public static NameValueCollection QueryString(this string url)
+        {
+            return HttpUtility.ParseQueryString(new Uri(url).Query);
+        }
+
+        // get single querystring value with specified key
+        public static string QueryString(this string url, string key)
+        {
+            return new Uri(url).QueryString()[key];
+        }
+    }
+}
