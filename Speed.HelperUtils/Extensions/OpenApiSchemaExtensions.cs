ccdc61aaa1fbc7ff4ec410666be592a2b32639a0
Sanket Naik <49100301+sankdeveloper@users.noreply.github.com>
2025-02-14 10:47:11 +0530

    Add files via upload

---
+++ Speed.HelperUtils/Extensions/OpenApiSchemaExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Writers;
+
+namespace Clover.Core.WebAPI.CustomPluginService.Extensions;
+
+public static class OpenApiSchemaExtensions
+{
+    public static async Task<string> ReadAsJsonStringAsync(this OpenApiSchema? openApiSchema)
+    {
+        if (openApiSchema is null)
+            return string.Empty;
+
+        await using var fileStream = new MemoryStream();
+        await using var writer = new StreamWriter(fileStream);
+        var writerSetting = new OpenApiWriterSettings() { InlineLocalReferences = true, InlineExternalReferences = true };
+
+        openApiSchema.SerializeAsV2WithoutReference(new OpenApiJsonWriter(writer, writerSetting));
+        await writer.FlushAsync();
+
+        return Encoding.UTF8.GetString(fileStream.ToArray());
+    }
+}
